35004ccc0761797b8f8571c77724e24b4fb15e4c
Rushi <189489rishi@gmail.com>
2022-03-05 18:03:20 +0530

    Add project files.

---
+++ MyBinaryTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Search_Tree
+{
+    class MyBinaryTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public MyBinaryTree<T>? LeftTree { get; set; }
+        public MyBinaryTree<T>? RightTree { get; set; }
+        public MyBinaryTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.RightTree = null;
+            this.LeftTree = null;
+        }
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new MyBinaryTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new MyBinaryTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        private int leftCount = 0, rightCount = 0;
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Create Binary Search Tree
+
+// Binary serach tree
+using System;
+
+namespace Binary_Search_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary search tree");
+
+            MyBinaryTree<int> myBinaryTree = new MyBinaryTree<int>(56);
+            myBinaryTree.Insert(56);
+            myBinaryTree.Insert(30);
+            myBinaryTree.Insert(70);
+
+            myBinaryTree.Display();
+
+        }
+    }
+}
